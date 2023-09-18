75b2c32262fed8d3b3832245b7adfcd530227a88
Mitchell Berry <foss@mitchellberry.com>
2023-09-18 03:53:32 +0000

    Added save button which opens link creation form Will also open on ctrl+v and paste the url Only shows url field at first, then autofills with title Has a 1500ms debounce to keep it from opening prematurely if the user is typing the address. Closes dialog form after link created

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,21 @@
     import { SvelteToast } from '@zerodevx/svelte-toast';
     import { readCurrentLinks } from './components/utils';
     import Loading from '~icons/line-md/loading-loop';
+    import { readText } from '@tauri-apps/api/clipboard';
 
     let mode: SortMode = 'normal';
     let initialFetch: Promise<LinkInfo[]>;
+    let showForm = false;
+    let url = '';
+
+    async function onKeyDown(event: KeyboardEvent) {
+        // Open save dialog if ctrl+v is used but only if it's not already open
+        // auto-fills the url field
+        if (event.ctrlKey && event.key == 'v' && !showForm) {
+            url = await readText() ?? "";
+            showForm = !showForm;
+        }
+    }
 
     onMount(() => {
         initialFetch = readCurrentLinks(mode);
@@ -20,8 +32,11 @@
 </script>
 
 <div class="relative flex h-screen min-h-screen w-screen flex-col text-white">
-    <h1 class="fixed flex h-14 w-full items-center bg-neutral-800 px-8 text-lg font-medium">
-        ARK Shelf
+    <h1 class="fixed flex h-14 w-full items-center justify-between bg-neutral-800 px-8 text-lg font-medium">
+        ARK Shelf 
+        <button on:click={() => (showForm = !showForm)} class="text-white px-4 py-2 rounded-md ml-4 border hover:bg-blue-400 border-blue-400">
+            Save
+        </button>
     </h1>
     <main class="absolute top-14 h-[calc(100vh-3.5rem)] w-screen">
         <div class="flex h-full overflow-hidden overflow-y-scroll bg-neutral-950 px-8 py-4">
@@ -37,7 +52,9 @@
                     {/each}
                 {/await}
             </div>
-            <Form />
+            {#if showForm}
+                <Form url={url} bind:show={showForm}/>
+            {/if}
         </div>
     </main>
 </div>
@@ -56,3 +73,5 @@
         --toastContainerLeft: auto;
     }
 </style>
+
+<svelte:window on:keydown={onKeyDown} />
--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -2,22 +2,27 @@
   import { toast } from '@zerodevx/svelte-toast';
   import { linksInfos } from '../store';
   import { createLink, getPreview } from './utils';
-
-  let url = '';
+  
+  export let url = '';
+  export let show = false;
   let titleElement: HTMLInputElement;
   let descriptionElement: HTMLInputElement;
+  let timer;
+  let showMeta = false;
 
-  $: disabled = !url;
-
-  const auto = async () => {
-    const graph = await getPreview(url);
-    if (graph) {
-      titleElement.value = graph.title;
-      descriptionElement.value = graph.description;
-    } else {
-      toast.push('Failed to fetch website data');
-    }
-  };
+  const debounceAuto = async () => {
+		clearTimeout(timer);
+		timer = setTimeout( async () => {
+      showMeta = true;
+      const graph = await getPreview(url);
+      if (graph) {
+        titleElement.value = graph.title;
+        descriptionElement.value = graph.description;
+      } else {
+        toast.push('Failed to fetch website data');
+      }
+		}, 1500);
+	}
 </script>
 
 <form
@@ -42,6 +47,7 @@
     } else {
       toast.push('Error creating link');
     }
+    show = false;
   }}
 >
   <label for="url" aria-label="URL" />
@@ -53,28 +59,30 @@
     placeholder="URL*"
     class="rounded-md bg-neutral-950 px-2 py-3 outline-none ring-1 ring-neutral-500"
     bind:value={url}
+    on:input={debounceAuto}
   />
-  <label for="title" aria-label="Title" />
-  <input
-    type="text"
-    id="title"
-    name="title"
-    required
-    placeholder="Title*"
-    bind:this={titleElement}
-    class="rounded-md bg-neutral-950 px-2 py-3 outline-none ring-1 ring-neutral-500"
-  />
-  <label for="description" aria-label="Optional description" />
-  <input
-    type="text"
-    name="description"
-    bind:this={descriptionElement}
-    placeholder="Description (Optional)"
-    class="rounded-md bg-neutral-950 px-2 py-3 outline-none ring-1 ring-neutral-500"
-    id="description"
-  />
+  {#if showMeta}
+    <label for="title" aria-label="Title" />
+    <input
+      type="text"
+      id="title"
+      name="title"
+      required
+      placeholder="Title*"
+      bind:this={titleElement}
+      class="rounded-md bg-neutral-950 px-2 py-3 outline-none ring-1 ring-neutral-500"
+    />
+    <label for="description" aria-label="Optional description" />
+    <input
+      type="text"
+      name="description"
+      bind:this={descriptionElement}
+      placeholder="Description (Optional)"
+      class="rounded-md bg-neutral-950 px-2 py-3 outline-none ring-1 ring-neutral-500"
+      id="description"
+    />
+  {/if}
   <div class="flex justify-between">
     <button type="submit" class="pl-2 text-blue-400">CREATE</button>
-    <button class="pr-2 text-rose-700" {disabled} on:click={auto}>AUTO FILLED</button>
   </div>
 </form>
