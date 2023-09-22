395217ef3442530f268d76929fc457fc03d44727
Gwendal Fernet <gwendalfernet@gmail.com>
2023-09-22 12:38:37 +0200

    preview after reload

--- src/components/LinkCard.svelte
+++ src/components/LinkCard.svelte
@@ -4,10 +4,12 @@
     import LinkFooter from './LinkFooter.svelte';
     import Title from './Title.svelte';
     import { Tooltip } from 'flowbite-svelte';
+    import { getPreview } from './utils';
 
     export let link: LinkInfo;
 
     let date = link.created_time ? new Date(link.created_time.secs_since_epoch * 1000) : new Date();
+    let preview = link.graph;
 
     const format = (value: number) => {
         return value.toString().padStart(2, '0');
@@ -22,6 +24,22 @@
         )}:${format(date.getMinutes())}:${format(date.getSeconds())}`;
     };
 
+    const getLinkPreview = async (link: LinkInfo) => {
+        if (link.graph) {
+            return link.graph;
+        }
+        const preview = await getPreview(link.url);
+        return {
+            imageUrl: preview?.image,
+            title: preview?.title,
+            description: preview?.description,
+        };
+    };
+
+    $: getLinkPreview(link).then(graph => {
+        preview = graph;
+    });
+
     $: created_time = displayCreation(link.created_time?.secs_since_epoch);
 </script>
 
@@ -34,18 +52,18 @@
     <LinkFooter name={link.name} url={link.url} score={link.score} />
 </div>
 <Tooltip placement="bottom" class="z-10 mx-2 max-w-[calc(100vw-2rem)] bg-neutral-500">
-    <p class="text-base">{link?.graph?.title ?? 'Preview may not be available at the moment'}</p>
+    <p class="text-base">{preview?.title ?? 'Preview may not be available at the moment'}</p>
     <div class="flex text-sm">
-        {#if link?.graph?.imageUrl}
+        {#if preview?.imageUrl}
             <img
-                src={link.graph.imageUrl}
+                src={preview.imageUrl}
                 alt="Preview of {link.url}"
                 width="100"
                 height="100"
                 loading="lazy" />
         {/if}
         <p class="break-all pl-2">
-            {link.desc ?? link.graph?.description ?? ''}
+            {link.desc ?? preview?.description ?? ''}
         </p>
     </div>
 </Tooltip>
